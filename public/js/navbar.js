6ae001d78d55508f13ab0251ca29463b0ea287db
Efrain Daza <dazaa94@hotmail.com>
2023-10-21 23:09:56 -0300

    agregando menu hamburgues, falta corregir

---
+++ public/js/navbar.js
@@ -0,0 +1,12 @@
+
+
+function openNav(){
+        document.getElementById("menuMobile").style.width = "100%";// que se abra al 100% el menu
+    }
+function closeNav(){
+        document.getElementById("menuMobile").style.width = "0%";// se cierra por que le pasamos un 0% de ancho!
+    }
+
+
+
+
