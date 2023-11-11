488d7b29fdd119cd4137901e2ec82a90aab12a30
Efrain Daza <dazaa94@hotmail.com>
2023-11-11 20:24:03 -0300

    carrusel

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -11,6 +11,25 @@ window.addEventListener("load", function(){
         nav.classList.remove("visible")
     })
 
+    /* carrusel*/
+
+    const caja = document.querySelector(".caja");    
+    const punto = document.querySelectorAll(".punto");
+
+    punto.forEach((cadaPunto,i) => {
+        punto[i].addEventListener("click",() => {
+           let posicion = i
+           let operacion = posicion * -16.66666666666667
+
+           caja.style.transform = `translateX(${ operacion }%)`
+
+           punto.forEach((cadaPunto,i) =>{
+            punto[i].classList.remove("activo")
+           })
+           punto[i].classList.add("activo")
+        })
+    });
+
 });
 
 
